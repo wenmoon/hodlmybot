24cf08f54f7f8301d3298c8460ba2d3cd0d98b2d
Magnus Nevstad <magnus.nevstad@eggsdesign.no>
2018-03-09 15:53:24 +0100

    moved credentials for twitter api into config file
    updated hodlcore

--- cron-twitter.py
+++ cron-twitter.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
-import time
+import json
 
 from hodlcore import model
 from hodlcore import api
 from hodlcore import db
 
 def main():
+    with open('api-creds-twitter.json', 'r') as file:
+        creds = model.OAuthCredentials(json.load(file))    
     database = db.TwitterDB()
     names = database.get_tracked()
     for name in names:
-        twitter = api.get_twitter(name)
+        twitter = api.get_twitter(name, creds)
         if twitter is not None:
             database.insert(twitter)
 
